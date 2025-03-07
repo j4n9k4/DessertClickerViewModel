d96a6e9d0c010ccf6c9384f71583d579c46507a4
j4n9k4 <janukananayakkara@gmail.com>
2025-03-08 00:48:39 +0530

    Finished App with a viewModel

--- app/src/main/java/com/example/dessertclicker/ui/ClickerUiState.kt
+++ app/src/main/java/com/example/dessertclicker/ui/ClickerUiState.kt
@@ -1,7 +1,12 @@
 package com.example.dessertclicker.ui
 
+import androidx.annotation.DrawableRes
+import com.example.dessertclicker.data.Datasource.dessertList
+
 data class ClickerUiState(
     val currentDessertIndex: Int = 0,
     val dessertsSold: Int = 0,
-    val revenue: Int = 0
+    val revenue: Int = 0,
+    @DrawableRes val currentDessertImage: Int = dessertList[currentDessertIndex].imageId,
+    val currentDessertPrice: Int = dessertList[currentDessertIndex].price
 )
